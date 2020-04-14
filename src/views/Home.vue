c7302e81eba318d01e0d5a747189b43137838a7f
Adeogun Damilola A <38430270+codepalmz@users.noreply.github.com>
2020-04-14 15:00:11 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <div class="home">
     <v-container>
       <div class="jumbotron myJumbo mt-n6 px-8 py-12 my-n9">
-        <h1 class="display-1 text-center">Create email lists from LinkedIn</h1>
+        <h1 class="display-1 text-center">Lorem ipsum dolor sit amet</h1>
         <p class="text-center lead text-break">
-          Wiza is magic. Turn any LinkedIn Sales Navigator search into a clean list of verified emails, ready for outreach.  <br/>
+          Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem  <br/>
           <v-btn rounded color="#F7E1D0" class="mt-3">Get Started</v-btn>
         </p>
-        <p class="text-center">Sign up today and export <strong>20 emails for free!</strong></p>
+        <p class="text-center">Lorem ipsum dolor sit amet <strong>Lorem ipsum dolor sit amet</strong></p>
         
       </div>
     </v-container>
